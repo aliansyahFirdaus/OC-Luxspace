8d67a8a2d220a6492cdf7b5b9ec1f48a75b137b2
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-06-18 10:50:43 +0700

    success styling not found page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+import React from "react";
+
+import Headers from "../parts/Headers";
+import AsideMenu from "../parts/AsideMenu";
+import Footer from "../parts/Footer";
+import ErrorPage from "../parts/ErrorPage";
+
+export default function NotFound() {
+  return (
+    <>
+      <Headers theme="black" />
+      <ErrorPage />
+      <AsideMenu />
+      <Footer />
+    </>
+  );
+}
